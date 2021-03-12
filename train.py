4799d64b0fd091d949f5acf4926be276e9c88f2c
tsakas96 <aristos_fox@hotmail.com>
2021-03-12 21:14:07 +0100

    calculate the test accuracy

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ print(f'We have {len(sketch_icon_indices)} samples.')
 # shuffle the indices and labels
 sketch_icon_indices, positive_labels = shuffle(sketch_icon_indices, positive_labels)
 
-NEGATIVE_PAIRS_LEN = 10
+NEGATIVE_PAIRS_LEN = 1
 
 class Dataset():
     def __init__(self, indices, labels):
@@ -47,7 +47,7 @@ class Dataset():
 
         return indices, label
 
-def create_datasets(sketch_icon_indices, labels, dataset_class, p_train=0.8, p_test=0.2):
+def create_datasets(sketch_icon_indices, labels, dataset_class, p_train=0.9, p_test=0.1):
     len_data = len(sketch_icon_indices)
 
     # Define partition sizes
@@ -101,7 +101,7 @@ margin = 1
 optimizer = tf.keras.optimizers.Adam(0.0001)
 tf.keras.backend.set_floatx('float32')
 siameseModel = mynet()
-loss = siamese_loss
+loss = contrastive_loss
 
 def train_step( icons, sketches , labels, margin):
     with tf.GradientTape() as tape:
@@ -111,10 +111,35 @@ def train_step( icons, sketches , labels, margin):
         tape.watch(model2)
         labels = tf.convert_to_tensor(labels, dtype=tf.float32)
         tape.watch(labels)
-        current_loss = loss(model1, model2, labels)
+        current_loss = loss(model1, model2, labels, margin)
     grads = tape.gradient(current_loss, siameseModel.trainable_variables)
     optimizer.apply_gradients(zip(grads, siameseModel.trainable_variables))
     return current_loss
+
+# test_icon_indeces = set()
+# test_sketch_indices = set()
+# sketch_index = 0
+# for s, i in test_set.indices:
+#     test_sketch_indices.add(s)
+#     test_icon_indeces.add(i)
+
+# sketch_index =  np.random.choice(list(test_sketch_indices))
+
+
+# test_sketch = sketches[sketch_index]
+# s = test_sketch
+# test_sketch = np.expand_dims(test_sketch, 0)
+
+# test_icon_list = []
+# for i in test_icon_indeces:
+#     test_icon_list.append(icons[i])
+
+# test_icons = np.array(test_icon_list)
+# print(test_icons.shape)
+# plt.imshow(s)
+# plt.xticks([])
+# plt.yticks([])
+# plt.show()
 
 test_icon_indeces = set()
 test_sketch_indices = set()
@@ -126,20 +151,21 @@ for s, i in test_set.indices:
 sketch_index =  np.random.choice(list(test_sketch_indices))
 
 
-test_sketch = sketches[sketch_index]
-s = test_sketch
-test_sketch = np.expand_dims(test_sketch, 0)
+test_sketch_list = []
+test_sketch_name = []
+for s in test_sketch_indices:
+    test_sketch_list.append(sketches[s])
+    test_sketch_name.append(sketch_names_array[s])
+test_sketches = np.array(test_sketch_list)
+print(test_sketches.shape)
 
 test_icon_list = []
+test_icon_name = []
 for i in test_icon_indeces:
     test_icon_list.append(icons[i])
-
+    test_icon_name.append(icons_name_cat[i][0])
 test_icons = np.array(test_icon_list)
 print(test_icons.shape)
-plt.imshow(s)
-plt.xticks([])
-plt.yticks([])
-plt.show()
 
 for epoch in range(num_epochs):
     count = 0
@@ -149,30 +175,74 @@ for epoch in range(num_epochs):
         loss_value = train_step(i, s, labels, margin)
         epoch_loss_avg.update_state(loss_value)
         count = count + 1
-        print(count)
+        #print(count)
     print("Epoch {:d}: Loss: {:.3f}".format(epoch,epoch_loss_avg.result()))
     if epoch%5==0:
-    
-        sketch_repr = siameseModel(test_sketch)
-        print(sketch_repr.shape)
-        sketch_representations = np.tile(sketch_repr, len(test_icons)).reshape(len(test_icons), 64)
-        print(sketch_representations.shape)
-        
+        acc_1 = 0
+        acc_10 = 0
+        sketch_representations = []
+        for i in range(len(test_sketches)):
+            sketch_repr =  siameseModel(np.expand_dims(test_sketches[i], 0))
+            sketch_representations.append(sketch_repr)
+        sketch_representations = np.vstack(sketch_representations)
+
         icon_representations = []
         for i in range(len(test_icons)):
             icon_repr =  siameseModel(np.expand_dims(test_icons[i], 0))
             icon_representations.append(icon_repr)
         icon_representations = np.vstack(icon_representations)
 
-        diff = np.sqrt(np.mean((sketch_representations - icon_representations)**2, -1))
-        
-        top_k = np.argsort(diff)[:20]
-        #print ('##' + str(epoch) + ' : loss == ' + str(loss_))
+        for i in range(len(sketch_representations)):
+            sketch_repr = sketch_representations[i]
+            sketch_representations_tile = np.tile(sketch_repr, len(test_icons)).reshape(len(test_icons), 64)
+            diff = np.sqrt(np.mean((sketch_representations_tile - icon_representations)**2, -1))
+            top_k = np.argsort(diff)[:10]
+            
+            for j in range(len(top_k)):
+                index = top_k[j]
+                if i == 0 and test_sketch_name[i] == test_icon_name[index]:
+                    acc_1 = acc_1 + 1
+                    acc_10 = acc_10 + 1
+                    break
+                elif test_sketch_name[i] == test_icon_name[index]:
+                    acc_10 = acc_10 + 1
+                    break
+        print("Accurance of top 1: " + str(acc_1/len(test_sketches)))
+        print("Accurance of top 10: " + str(acc_10/len(test_sketches)))
 
-        plt.figure(figsize=(20, 20))
-        for i in range(20):    
-            plt.subplot(1, 20, i+1)
-            plt.imshow(test_icons[top_k[i]])
-            plt.xticks([])
-            plt.yticks([])
-        plt.show()
+
+# for epoch in range(num_epochs):
+#     count = 0
+#     epoch_loss_avg = tf.keras.metrics.Mean()
+#     for _,(indices, labels) in enumerate(trainloader):
+#         i, s = get_batch(indices, icons, sketches)
+#         loss_value = train_step(i, s, labels, margin)
+#         epoch_loss_avg.update_state(loss_value)
+#         count = count + 1
+#         print(count)
+#     print("Epoch {:d}: Loss: {:.3f}".format(epoch,epoch_loss_avg.result()))
+#     if epoch%5==0:
+    
+#         sketch_repr = siameseModel(test_sketch)
+#         print(sketch_repr.shape)
+#         sketch_representations = np.tile(sketch_repr, len(test_icons)).reshape(len(test_icons), 64)
+#         print(sketch_representations.shape)
+        
+#         icon_representations = []
+#         for i in range(len(test_icons)):
+#             icon_repr =  siameseModel(np.expand_dims(test_icons[i], 0))
+#             icon_representations.append(icon_repr)
+#         icon_representations = np.vstack(icon_representations)
+
+#         diff = np.sqrt(np.mean((sketch_representations - icon_representations)**2, -1))
+        
+#         top_k = np.argsort(diff)[:20]
+#         #print ('##' + str(epoch) + ' : loss == ' + str(loss_))
+
+#         plt.figure(figsize=(20, 20))
+#         for i in range(20):    
+#             plt.subplot(1, 20, i+1)
+#             plt.imshow(test_icons[top_k[i]])
+#             plt.xticks([])
+#             plt.yticks([])
+#         plt.show()
